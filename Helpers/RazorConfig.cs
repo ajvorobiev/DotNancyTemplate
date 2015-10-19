fbeb6eae468a7032d974c470f2d195cd93f6e1c3
Alex Vorobiev <vorobiev.a.j@gmail.com>
2015-10-19 09:08:14 +0200

    Switched to razor

---
+++ Helpers/RazorConfig.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNancyTemplate.Helpers
+{
+    using System.Collections.Generic;
+    using Nancy.ViewEngines.Razor;
+
+    public class RazorConfig : IRazorConfiguration
+    {
+        public IEnumerable<string> GetAssemblyNames()
+        {
+            yield return "DotNancyTemplate.Entities";
+            yield return "DotNancyTemplate.Models";
+        }
+
+        public IEnumerable<string> GetDefaultNamespaces()
+        {
+            yield return "DotNancyTemplate.Entities";
+            yield return "DotNancyTemplate.Models";
+        }
+
+        public bool AutoIncludeModelNamespace
+        {
+            get { return true; }
+        }
+    }
+}
